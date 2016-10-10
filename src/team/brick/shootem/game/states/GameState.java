f7eeb3bb71bcad5f97a6d5431d4ca6c06dbec29a
wesleys92 <wsshall@gmail.com>
2016-10-10 12:37:24 -0400

    Add files via upload

--- src/team/brick/shootem/game/states/GameState.java
+++ src/team/brick/shootem/game/states/GameState.java
@@ -17,7 +17,7 @@ import team.brick.shootem.game.ui.ScoreCounter;
 public class GameState extends State {
 	
 	private World world;
-	private ScoreCounter scoreCounter = new ScoreCounter(0); //Creates a variable using the s
+	private ScoreCounter score = new ScoreCounter(0); //Initializes Score Counter
 	
 	public GameState(Handler handler){
 		super(handler);
@@ -38,8 +38,7 @@ public class GameState extends State {
 		g.setColor(Color.green);
 		g.fillRect(0, 720, 500, 30);
 		g.setColor(Color.black);
-		String score = String.valueOf(scoreCounter.getScore());
-		g.drawString(score, 240, 740);
+		g.drawString(String.valueOf(score.getScore()), 240, 740); //Displays the score
 		g.drawString("LIVES", 450, 740);	//Lives placeholder
 		g.drawString("LEVEL", 25, 740);		//Level number placeholder
 	}
