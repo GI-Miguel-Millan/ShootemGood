96b8fa47f834ea069496810aa0b47071874d5fb2
wesleys92 <wsshall@gmail.com>
2016-10-17 01:03:42 -0400

    Add files via upload

--- src/team/brick/shootem/game/ui/UILabel.java
+++ src/team/brick/shootem/game/ui/UILabel.java
@@ -2,34 +2,33 @@ package team.brick.shootem.game.ui;
 
 import java.awt.Graphics;
 
-public class UILabel extends UIObject
-{
+public class UILabel extends UIObject {
 
-	private String label;
+	private String text;
 	
-	public UILabel(float x, float y, int width, int height, String label)
-	{
+	public UILabel(float x, float y, int width, int height, String text, ClickListener clicker) {
 		super(x, y, width, height);
-		this.label = label;
+		this.text = text;
 	}
 
 	@Override
 	public void tick() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void render(Graphics g)
-	{
-		g.drawString(label, (int) x, (int) y); 
-		
-	}
-
-	@Override
-	public void onClick() {
-		// TODO Auto-generated method stub
 		
 	}
 	
+	@Override
+	public void render(Graphics g) {
+		g.drawString(text, (int)x, (int)y);
+	}
+
+	@Override
+	public void onClick()
+	{
+
+	}
+	
+	public void setText(String txt){
+		text = txt;
+	}
+
 }
