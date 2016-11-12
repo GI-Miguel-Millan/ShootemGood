1b4cd77a99e63aaab1cb12f2f50d1a546cd8b34a
Miguel Millan <miggy221@gmail.com>
2016-11-11 23:58:23 -0500

    Merge current build into master from Interactions

---
+++ src/team/brick/shootem/game/entities/creatures/enemies/LesserInterceptor.java
@@ -0,0 +1,12 @@
+package team.brick.shootem.game.entities.creatures.enemies;
+
+import team.brick.shootem.game.Handler;
+
+public class LesserInterceptor extends Interceptor{
+
+	public LesserInterceptor(Handler handler, float x, float y) {
+		super(handler, x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+}
